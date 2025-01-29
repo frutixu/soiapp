c88ebda4c3c4fadcc5e76ed81e1b885206d73a3d
frutixu <francois.urb@gmail.com>
2025-01-29 23:52:57 +0100

    Add Streamlit app code

---
+++ stock_comparison.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+# Define the tickers for Soitec and Nvidia
+tickers = ['SOI.PA', 'NVDA']
+
+# Fetch data for the past 20 years from today
+data = yf.download(tickers, start="2003-01-01")
+
+# Extract closing prices
+close_prices = data['Close']
+close_prices.columns = ['SOITEC', 'NVDA']
+
+
+# Plotting using plotly-express for interactivity
+fig = px.line(close_prices, title='Stock Price Comparison: Soitec SA (SOIT) vs Nvidia (NVDA)', labels={'value':'Closing Price', 'index': 'Date'})
+st.plotly_chart(fig)
