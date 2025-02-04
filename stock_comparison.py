cd94fa12210e7bda1c35d015b4875b367d8421a6
frutixu <131778429+frutixu@users.noreply.github.com>
2025-02-04 19:16:32 +0100

    Update stock_comparison.py

--- stock_comparison.py
+++ stock_comparison.py
@@ -3,21 +3,21 @@ import yfinance as yf
 import pandas as pd
 import plotly.express as px
 
-Define the tickers for Soitec, Nvidia, MSFT, AAPL, BTC, and ATOS
+#Define the tickers for Soitec, Nvidia, MSFT, AAPL, BTC, and ATOS
 all_tickers = ['SOI.PA', 'NVDA', 'MSFT', 'AAPL', 'ATOS']
 
-Function to fetch data for the past 20 years from today
+#Function to fetch data for the past 20 years from today
 def get_data(tickers):
 data = yf.download(tickers, start="2022-01-01")
 return data['Close']
 
-Fetch and process the data
+#Fetch and process the data
 close_prices = get_data(all_tickers)
 
-Main title of the page
+#Main title of the page
 st.title('Le Jean Yves Chart: SOI et le reste')
 
-Display a dropdown to select companies for comparison
+£#Display a dropdown to select companies for comparison
 selected_companies = st.multiselect('Select Companies', all_tickers, default=all_tickers[:2])
 if not selected_companies:
 st.error("Please select at least one company to compare.")
